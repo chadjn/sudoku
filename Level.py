8a00a5819bd0c70cfee6697e3aa12846f9162e2a
Charlotte DUJARDIN <charlotte.b.dujardin@gmail.com>
2019-10-23 11:40:57 +0200

    Classes écrites par Lucille (avant utilisation commune git)

--- Level.py
+++ Level.py
@@ -1,1 +1,16 @@
-class Level():
+"""
+Mardi 22 octobre 2019
+Lucille Gueguen
+"""
+
+# classe correspondant au niveau de jeu de la grille de SUDOKU
+class Level:
+
+    def __init__(self, number):
+        self.number = number
+
+    def getNumber(self):
+        return self.number
+
+    def setNumber(self, new_number):
+        self.number = new_number
--- Timer.py
+++ Timer.py
@@ -1,1 +1,23 @@
-class Timer():
+"""
+Mardi 22 octobre 2019
+Lucille Gueguen
+"""
+
+# classe mettant en place le timer du SUDOKU
+class Timer:
+
+    def __init__(self, secondes, minutes):
+        self.secondes = secondes
+        self.minutes = minutes
+
+    def getSecondes(self):
+        return self.secondes
+
+    def setSecondes(self, new_secondes):
+        self.secondes = new_secondes
+
+    def getMinutes(self):
+        return self.minutes
+
+    def setMinutes(self, new_minutes):
+        self.minutes = new_minutes
