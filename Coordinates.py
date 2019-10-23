a4dc0d229eec3bb8a4821d38779e75e62d872657
Charlotte DUJARDIN <charlotte.b.dujardin@gmail.com>
2019-10-23 11:39:11 +0200

    Classes écrites par Léa (avant utilisation commune git)

---
+++ Coordinates.py
@@ -0,0 +1,25 @@
+"""
+Léa Dollé
+Mardi 22 octobre 2019
+classe coordonnées
+"""
+
+# Léa
+# classe permettant de lire les coordonnées attribuées au cases
+class Coordinates:
+
+    def __init__(self, x, y):  # initialisation des coordonnées dans la classe
+        self.x = x
+        self.y = y
+
+    def getX(self):  # fonction pour que la classe lise X
+        return self.x
+
+    def setX(self, new_x):  # fonction pour changer la valeur de x
+        self.new_x
+
+    def getY(self):
+        return self.y
+
+    def setY(self, new_y):
+        self.y = new_y
--- Player.py
+++ Player.py
@@ -1,7 +1,23 @@
-import Level
-from Level import *
+"""
+Mardi 22 octobre 2019
+Léa Dollé
+"""
 
-import Timer
-from Timer import *
+# classe définissant le joueur du sudoku
+class Player:
 
-class Player():
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def getName(self):
+        return self.name
+
+    def setName(self, new_name):
+        self.name = new_name
+
+    def getScore(self):
+        return self.score
+
+    def setScore(self, new_score):
+        self.score = new_score
